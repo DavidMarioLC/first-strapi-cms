3e8d2d8edc444a9dc7f9e177f9338878d7c8cb36
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-03-04 07:05:01 -0500

    feat: update PostgreSQL configuration and implement GraphQL plugin settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,20 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true, // Esto habilita el playground en producción
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
 
 export default config;
